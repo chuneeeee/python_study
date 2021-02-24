33ef74175f46349059e163b4bde1fbe0709808a3
chuneeeee <charie96@naver.com>
2021-02-24 13:37:36 +0900

    test

---
+++ Feb_week4/greedy_lv2.py
@@ -0,0 +1,18 @@
+def solution(people, limit):
+    answer = 0
+    check=[0 for _ in people]
+
+            
+    people.sort(reverse=True)
+    answer=len(people)
+    start=0
+    tmp=0
+    end=len(people)-1
+    while start<end:
+        if people[start]+people[end]<=limit:
+            start+=1
+            end-=1
+            tmp+=1
+        else:
+            start+=1
+    return answer-tmp
---
+++ Feb_week4/heap_lv2.py
@@ -0,0 +1,19 @@
+import heapq
+# heapq 를 사용하여 힙구현 
+def solution(scoville, K):
+    answer=0
+    heap=[]
+    for i in scoville:
+        heapq.heappush(heap,i)
+    while heap[0]<K :
+        
+        if len(heap)>=2:
+            a=heapq.heappop(heap)
+            b=heapq.heappop(heap)
+        
+            heapq.heappush(heap,a+(b*2))
+            answer+=1
+        else:
+            return -1
+    
+    return answer
